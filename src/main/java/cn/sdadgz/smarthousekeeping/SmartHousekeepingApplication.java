c559d53b6ba2f26af5701dad19ef40247747699c
sdadgz <pengchengma5@gmail.com>
2023-05-07 15:56:36 +0800

    init

---
+++ src/main/java/cn/sdadgz/smarthousekeeping/SmartHousekeepingApplication.java
@@ -0,0 +1,13 @@
+package cn.sdadgz.smarthousekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHousekeepingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHousekeepingApplication.class, args);
+    }
+
+}
